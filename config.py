9e87f6c873fbe6c0b673383e42c27057fc723663
Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>
2024-03-29 16:07:42 +0530

    Update config.py
    Signed-off-by: Ddwsukuna <97271881+Harshmanjhi@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+    "START_IMG_URL", "https://graph.org/file/e1a2994f6ade384a62961.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+    "PING_IMG_URL", "https://graph.org/file/e1a2994f6ade384a62961.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
